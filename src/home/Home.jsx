4c323b33eae5a50eded132b97cace374e4ba4dc4
SAMUEL-OBIEKWUGO <samuelchidera127@gmail.com>
2024-06-25 11:34:27 -0700

    did the function

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,16 +2,24 @@
 import "../styles/home.css"
 import pattern from "../assets/pattern-divider-desktop.svg"
 import dice from "../assets/icon-dice.svg"
+import { useState } from "react"
 
 const Home = () => {
+   const [isToggled, setisToggled] = useState(false)
+   const  handleClick = ()=>{
+      setisToggled(prevState => !prevState);
+   };
+
   return (
    <>
    <main id='body'>
     <div id="advise">
       <h2 className="advise">ADVISE #117</h2>
-      <p><q>It is easy to sit up and take <br /> notice, what&apos;s difficult is getting <br /> up and taking action.</q></p>
+      
+      <p>{isToggled ? `"It is easy to sit up and take notice, what's difficult is getting up and taking action."`:`"I t is easy to have big dreams, but what's challenging is turning those dreams into reality."`}</p>
+      {/* <p>{text}</p> */}
       <img src={pattern} alt="" width={390} id="pattern"/>
-      <button id="dice" >
+      <button id="dice" onClick={handleClick}>
       <img src={dice} alt=""/>
       </button>
     </div>
