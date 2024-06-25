4719a7ef12c7ecdeb87061f8b8fb25439dd35d99
SAMUEL-OBIEKWUGO <samuelchidera127@gmail.com>
2024-06-25 11:40:19 -0700

    remove the space

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     <div id="advise">
       <h2 className="advise">ADVISE #117</h2>
       
-      <p>{isToggled ? `"It is easy to sit up and take notice, what's difficult is getting up and taking action."`:`"I t is easy to have big dreams, but what's challenging is turning those dreams into reality."`}</p>
+      <p>{isToggled ? `"It is easy to sit up and take notice, what's difficult is getting up and taking action."`:`"It is easy to have big dreams, but what's challenging is turning those dreams into reality."`}</p>
       {/* <p>{text}</p> */}
       <img src={pattern} alt="" width={390} id="pattern"/>
       <button id="dice" onClick={handleClick}>
