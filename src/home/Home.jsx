ecc4b947bd2b38bdbf0aa8deca3ff576ab4bec8c
SAMUEL-OBIEKWUGO <samuelchidera127@gmail.com>
2024-06-26 11:31:20 -0700

    fixed the api

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,11 @@
-// import React from 'react'
+import React, { useEffect, useState } from "react";
 import "../styles/home.css";
 import pattern from "../assets/pattern-divider-desktop.svg";
 import patternMobile from "../assets/pattern-divider-mobile.svg";
 import dice from "../assets/icon-dice.svg";
-import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [data, setData] = useState(null);
+  const [advice, setAdvice] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -18,12 +17,12 @@ const Home = () => {
     fetch("https://api.adviceslip.com/advice")
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network is bad");
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then((data) => {
-        setData(data);
+      .then((advice) => {
+        setAdvice(advice.slip);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -33,21 +32,27 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <section className="container">
-        <main id="body">
-          <div id="advise">
-            <h2 className="advise">ADVISE #117</h2>
-            <p>{JSON.stringify(data)}</p>
-            <img src={pattern} alt="" width={390} id="pattern" />
-            <img src={patternMobile} alt="" id="patternMobile" />
-            <button id="dice" onClick={handleClick}>
-              <img src={dice} alt="" />
-            </button>
-          </div>
-        </main>
-      </section>
-    </>
+    <section className="container">
+      <main id="body">
+        <div id="advise">
+          {isLoading ? (
+            <h2 className="advise">Loading...</h2>
+          ) : error ? (
+            <h2 className="advise">Error: {error.message}</h2>
+          ) : (
+            <>
+              <h2 className="advise">ADVICE #{advice.id}</h2>
+              <p>{advice.advice}</p>
+              <img src={pattern} alt="" width={390} id="pattern" />
+              <img src={patternMobile} alt="" id="patternMobile" />
+              <button id="dice" onClick={handleClick}>
+                <img src={dice} alt="" />
+              </button>
+            </>
+          )}
+        </div>
+      </main>
+    </section>
   );
 };
 
